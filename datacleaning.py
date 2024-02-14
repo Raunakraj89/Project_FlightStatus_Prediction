54c724dfca866c8ec5f3caa15f968c1d984a8898
Paresh <yparesh@gmail.com>
2024-02-13 23:17:53 -0500

    Fixed bugs with data cleaning module

--- datacleaning.py
+++ datacleaning.py
@@ -1,5 +1,13 @@
 def doclean(spark, input_gcsfile, bq_table):
+    project = "spring-asset-408702"
+    dataset = "stg"
+
     df = spark.read.format("csv").option("path", input_gcsfile).option("header", "true").option("inferSchema", "true").load()
 
     df.show()
-    #dfclean = df.select("dep", "arr").filter(df.dep != "")
+    dfclean = df.select("empid", "depid", "salary").filter(df.depid.isNotNull())
+    dfclean.show()
+    dfclean.write.format("bigquery").option("temporaryGcsBucket","tempbucketparesh").option("table", "{}:{}.{}".format(project, dataset, bq_table)).option("mode", "append").save()
+
+    #dfclean.write.format("bigquery").option("table", "spring-asset-408702:stg.clean_data").option("temporaryGcsBucket","gs://tempbucketparesh").option("mode",                                                                                                  "append").save()
+
